4596b3764c4345014f9fb67080a01e0df520fbff
Andre Lee <andrelee@qnap.com>
2014-06-06 18:23:43 +0800

    i18n

---
+++ js/app/main.js
@@ -0,0 +1,56 @@
+function MainCtrl($rootScope) {
+    var getCurrentLang, getQueryParam;
+
+    getQueryParam = function(name) {
+      var currentQueryParams;
+      currentQueryParams = new URI().query(true);
+      return currentQueryParams[name];
+    };
+
+    window.getQueryParam = getQueryParam;
+
+    getCurrentLang = function() {
+      var lang;
+      lang = getQueryParam("lang");
+      if (lang) {
+        return lang.replace("-", "_");
+      } else {
+        var lang_browser = getCurrentLangByBrowserSettings();
+        console.log('browser lang:', lang_browser);
+        switch(lang_browser){
+            case 'zh':
+            case 'zh_TW':
+                return 'zh_TW';
+                break;
+            default:
+                return 'en';
+        }
+      }
+    };
+
+    getCurrentLangByBrowserSettings = function(){
+        var lang = window.navigator.userLanguage || window.navigator.language;
+        return lang;
+    }
+
+    window.getCurrentLang = getCurrentLang;
+
+
+    getCurrentYear = function(){
+        return new Date().getFullYear();
+    }
+
+    $rootScope.lang = getCurrentLang();
+
+    $rootScope.current_year = getCurrentYear();
+
+    $rootScope._ = function(key){
+        var lang = getCurrentLang();
+
+        if (LANGPACK && LANGPACK[lang] && LANGPACK[lang][key]){
+            return LANGPACK[lang][key];
+        }else{
+            return key;
+        }
+    };
+}
